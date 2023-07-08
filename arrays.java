e9b267464e459e60f8ba94f8a0ae640a605c88c9
Akash Kamat <100783004+AkashKamatCreates@users.noreply.github.com>
2023-07-08 11:41:48 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-      
-        int n = 10; //size of array
+
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt(); //taking array size from user
+        int n = 10; //size of array declared instead of taking from user 
         int[] arrname = new int[n];
         System.out.println(Arrays.toString(arrname));
 
