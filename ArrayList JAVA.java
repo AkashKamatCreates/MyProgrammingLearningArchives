c75acb92d46eab3af7e296a5447b02879f851825
Akash Kamat <100783004+AkashKamatCreates@users.noreply.github.com>
2023-07-08 10:52:23 +0530

    Create ArrayList JAVA.java

---
+++ ArrayList JAVA.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //syntax to declare and add elements to arraylist:
+        ArrayList<Integer> akash = new ArrayList<Integer>();
+        akash.add(13);
+        akash.add(24);
+        akash.add(36);
+
+        System.out.println(akash);
+
+      
+
+        //using arraylist as normal array: specify the size of the arraylist in the brackets...
+
+        ArrayList<Integer> akash = new ArrayList<Integer>(10); //this will act as a normal array  with 10 size
+        ArrayList<Integer> akash = new ArrayList<Integer>(n);  //this will act as normal array with size n
+
+    }
+}
