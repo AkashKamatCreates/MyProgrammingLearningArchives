87ca77fa3dc6597cf18c40e8364cbb1c1397ff34
Akash Kamat <100783004+AkashKamatCreates@users.noreply.github.com>
2023-07-08 11:30:11 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+      
+        int n = 10; //size of array
+        int[] arrname = new int[n];
+        System.out.println(arrname);
+
+    }
+}
