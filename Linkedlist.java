80da09c90aea277be17970e61a710420c7130023
Akash Kamat <100783004+AkashKamatCreates@users.noreply.github.com>
2024-05-13 11:07:49 +0530

    Update Linkedlist.java

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,6 +1,13 @@
 package org.example;
 import java.util.*;
 
+    /*
+     * types of linkedlist:
+     *  - singly linkedlist (one direction)
+     *  - doubly linkedlist (travells both directions)
+     *  - circular linkedlist (last node and first node are the same)
+     */
+
 //singly linked list:
 class listnode {
     int value;
@@ -28,6 +35,54 @@ class doublylistnode {
 
 public class LinkedList1 {
     public static void main(String[] args) {
+
+        //old code: 
+        LinkedList<String> list = new LinkedList<>();
+        
+        //adding nodes to the linkedlist
+        //addFirst adds to the start of the linkedlist
+        list.addFirst("akash");
+        list.addFirst("kamat");
+        list.addFirst("this is first node");
+        list.addFirst("sample to be removed");
+
+        //adding to the last of the linkedlist
+        //.add and .addLast are basically the same, adding elements to the last.
+        list.add("this is added at the last");
+        list.addLast("last addition using addLast");
+
+        //printing the linked list
+        System.out.println(list);
+
+        //printing linked list size
+        System.out.println(list.size());
+
+        //iterating through the list
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        //Searching for an element in linkedlist
+        if(list.contains("kamat")){
+                System.out.println("yes");
+        }
+        
+        //removing from the linkedlist 
+        System.out.println(list);
+        list.removeFirst(); //removes first element of the list
+        System.out.println(list);
+        list.removeLast(); //removes the last element of the list
+        System.out.println(list);
+        list.remove(3); //removes element on the index. 
+        System.out.println(list);
+
+        
+        /**
+        *
+        * NEW CODE HEREON:
+        *
+        */
+        //NEW CODE:        
         LinkedList<Integer> list = new LinkedList<>();
 
         // Adding elements to the LinkedList
